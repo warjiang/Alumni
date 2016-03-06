b078fae23aecff1f1acfaed39483aa0cc0ee6d41
Pengcheng Zhou <pczhou@seu.edu.cn>
2016-03-06 20:42:17 +0800

    增加九宫格图片缩放

--- app/src/main/java/cn/edu/seu/alumni/adapter/DynamicItemAdapter.java
+++ app/src/main/java/cn/edu/seu/alumni/adapter/DynamicItemAdapter.java
@@ -1,6 +1,10 @@
 package cn.edu.seu.alumni.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +30,7 @@ public class DynamicItemAdapter extends BasisAdapter<DynamicItem, DynamicItemAda
         context = mContext;
 
         for (int i = 0; i < images.length; ++i) {
-            images[i] = R.drawable.logo;
+            images[i] = R.drawable.test_image;
         }
     }
 
@@ -35,7 +39,7 @@ public class DynamicItemAdapter extends BasisAdapter<DynamicItem, DynamicItemAda
         context = mContext;
 
         for (int i = 0; i < images.length; ++i) {
-            images[i] = R.drawable.logo;
+            images[i] = R.drawable.test_image;
         }
     }
 
@@ -110,10 +114,65 @@ public class DynamicItemAdapter extends BasisAdapter<DynamicItem, DynamicItemAda
             } else {
                 imageView = (ImageView) convertView;
             }
-            imageView.setImageResource(images[position]);
+            DisplayMetrics dm = imageView.getResources().getDisplayMetrics();
+            int screenWidthDip = dm.widthPixels;
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeResource(imageView.getResources(), images[position], options);
+            int imageHeight = options.outHeight;
+            int imageWidth = options.outWidth;
+
+            if (1 == images.length || 2 == images.length) {
+                int x = imageHeight * screenWidthDip / imageWidth;
+                x = x < screenWidthDip ? x : screenWidthDip;
+                x /= images.length;
+                imageView.setImageBitmap(decodeSampledBitmapFromResource(imageView.getResources(), images[position], x, x));
+            } else {
+                imageView.setScaleType(ImageView.ScaleType.CENTER);
+                imageView.setImageBitmap(decodeSampledBitmapFromResource(imageView.getResources(),
+                        images[position], screenWidthDip / 3, screenWidthDip / 3));
+            }
+
             return imageView;
         }
 
+        private int calculateInSampleSize(
+                BitmapFactory.Options options, int reqWidth, int reqHeight) {
+            // 原始图片的宽高
+            final int height = options.outHeight;
+            final int width = options.outWidth;
+            int inSampleSize = 1;
+
+            if (height > reqHeight || width > reqWidth) {
+
+                final int halfHeight = height / 2;
+                final int halfWidth = width / 2;
+
+                while ((halfHeight / inSampleSize) > reqHeight
+                        || (halfWidth / inSampleSize) > reqWidth) {
+                    inSampleSize *= 2;
+                }
+            }
+
+            return inSampleSize;
+        }
+
+        private Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                       int reqWidth, int reqHeight) {
+
+            // 首先设置 inJustDecodeBounds=true 来获取图片尺寸
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeResource(res, resId, options);
+
+            // 计算 inSampleSize 的值
+            options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+            // 根据计算出的 inSampleSize 来解码图片生成Bitmap
+            options.inJustDecodeBounds = false;
+            return BitmapFactory.decodeResource(res, resId, options);
+        }
     }
 
     public static class viewHolder {
