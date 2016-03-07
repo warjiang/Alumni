4c21adeaadda3dfc13f9dddced1bbc6eed5ec767
zhikaizhang <zhangzhikai@seu.edu.cn>
2016-03-07 11:01:42 +0800

    WelcomeActivity界面重制，增加跳过按钮

--- app/src/main/java/cn/edu/seu/alumni/activity/WelcomeActivity.java
+++ app/src/main/java/cn/edu/seu/alumni/activity/WelcomeActivity.java
@@ -2,10 +2,23 @@ package cn.edu.seu.alumni.activity;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.OnClick;
 import cn.edu.seu.alumni.R;
 
 public class WelcomeActivity extends BaseActivity{
+
+    private Jump runnable;
+
+    @Bind(R.id.skip_button)
+    Button skipButton;
+
+    @OnClick(R.id.skip_button)
+    public void skipButtonOnClick(){
+        runnable.skip();
+    }
 
     @Override
     protected int getContentViewId() {
@@ -19,13 +32,50 @@ public class WelcomeActivity extends BaseActivity{
 
     @Override
     protected void initial() {
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                jumpThenFinish(RegisterActivity.class);
+        runnable = new Jump(2000);
+        new Thread(runnable).start();
+    }
+
+
+
+    private class Jump implements Runnable{
+
+        private long waitTime;
+        private long startTime;
+        private boolean running;
+        private Handler handler;
+
+        public Jump(long waitTime) {
+            this.waitTime = waitTime;
+            this.running = true;
+            this.startTime = System.currentTimeMillis();
+            this.handler = new Handler(Looper.getMainLooper());
+        }
+
+        @Override
+        public void run() {
+            while(running){
+                if(System.currentTimeMillis() - startTime >= waitTime){
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            jumpThenFinish(RegisterActivity.class);
+                        }
+                    });
+                    running = false;
+                }
             }
-        }, 2000);
+        }
+
+        public void skip(){
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    jumpThenFinish(RegisterActivity.class);
+                }
+            });
+            running = false;
+        }
     }
 
     @Override
