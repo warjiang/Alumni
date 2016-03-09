5df8898e0c197eed94275ac925ab100f3fdd0f52
warjiang <1096409085@qq.com>
2016-03-10 00:18:06 +0800

    人脉下的右上角+，以及新的好友

--- app/src/main/java/cn/edu/seu/alumni/activity/MainActivity.java
+++ app/src/main/java/cn/edu/seu/alumni/activity/MainActivity.java
@@ -195,6 +195,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 jump(PublishDynamicActivity.class);
                 break;
             case R.id.add_friend:
+                jump(AddFriendsActivity.class);
                 break;
         }
 
--- app/src/main/java/cn/edu/seu/alumni/fragment/ContactsFragment.java
+++ app/src/main/java/cn/edu/seu/alumni/fragment/ContactsFragment.java
@@ -20,6 +20,7 @@ import java.util.List;
 
 import butterknife.Bind;
 import cn.edu.seu.alumni.R;
+import cn.edu.seu.alumni.activity.NewFriendsActivity;
 import cn.edu.seu.alumni.adapter.ContactsListAdapter;
 import cn.edu.seu.alumni.javabean.Friend;
 import cn.edu.seu.alumni.util.CharacterParser;
@@ -219,6 +220,7 @@ public class ContactsFragment extends BaseFragment implements View.OnClickListen
         switch (v.getId()) {
             case R.id.contacts_new_friends_relative_layout:
             //    readyGo(NewFriendsActivity.class);
+                startActivity(new Intent(getContext(),NewFriendsActivity.class));
                 break;
             case R.id.contacts_alumni_good_relative_layout:
            //     readyGo(AlumniGoodActivity.class);
